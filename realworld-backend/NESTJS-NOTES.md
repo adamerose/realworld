a103759b27472a3363694bea56dfb3eda8c39a6b
Adam Rose <adrotog@gmail.com>
2023-02-26 15:37:03 -0500

    backend setup

---
+++ realworld-backend/NESTJS-NOTES.md
@@ -0,0 +1,18 @@
+# Setup
+
+```
+npm install -g @nestjs/cli
+nest new realworld-backend
+
+# Prisma https://docs.nestjs.com/recipes/prisma#prisma
+npm install prisma --save-dev
+npx prisma init
+npx prisma migrate dev --name init
+
+# Swagger https://docs.nestjs.com/openapi/introduction#setup-options
+npm install --save @nestjs/swagger
+```
+
+# Concepts
+
+# Prisma
--- realworld-backend/src/main.ts
+++ realworld-backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set up Swagger https://docs.nestjs.com/openapi/introduction#setup-options
+  const config = new DocumentBuilder()
+    .setTitle('RealWorld API')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  // Start application
   await app.listen(3000);
+  const url = 'http://localhost:3000/swagger';
+  console.log('Application is running on: ' + url);
 }
 bootstrap();
