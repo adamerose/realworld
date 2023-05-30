5948c6227ffc06e0d4243d2cf37419482e8e2559
Adam Rose <adrotog@gmail.com>
2023-05-30 15:33:45 -0400

    copy

--- realworld-backend/src/app.module.ts
+++ realworld-backend/src/app.module.ts
@@ -1,22 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
-    ConfigModule,
-    ArticlesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     AuthModule,
-    UsersModule,
+    UserModule,
+    BookmarkModule,
     PrismaModule,
-    ArticlesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- realworld-backend/src/main.ts
+++ realworld-backend/src/main.ts
@@ -1,26 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Set up Swagger https://docs.nestjs.com/openapi/introduction#setup-options
-  const config = new DocumentBuilder()
-    .setTitle('RealWorld API')
-    .setVersion('0.1')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
-
-  // Start application
-  await app.listen(3000);
-  const url = 'http://localhost:3000/swagger';
-  console.log('Application is running on: ' + url);
-
-  // Export swagger as json
-  // https://github.com/nestjs/swagger/issues/158#issuecomment-531560231
-  fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.listen(3333);
 }
 bootstrap();
